d31708303de526dd5903066dc3528ee6739b1ea9
Luan Pinheiro da Silva <lluanps@gmail.com>
2025-07-31 00:17:20 -0300

    add AdminLTE e criado algumas telas iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LayoutComponent } from './shared/layout/layout';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'dashboard', component: DashboardComponent },
+    ],
+  },
+  { path: '**', redirectTo: '' }
+];
